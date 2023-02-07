85b06897c887cd0f61a0752deaa6e7f554140238
1kivanc <kivanc0556@gmail.com>
2023-02-08 01:17:00 +0300

    template engine

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 const express = require('express');
-
+const ejs = require('ejs');
+const path = require('path');
 const app = express();
 
-const blog = {
-    id: 1, 
-    title: "Blog title",
-    description: "Blog description",
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
-}
-app.get('/', (req,res) => {
-    res.send(blog)
-})
-
+//MIDDLEWARES
+app.use(express.static('public'));
 const port = 3000;
+const blog = {
+  id: 1,
+  title: 'Blog title',
+  description: 'Blog description',
+};
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
-app.listen(port,() => {
-    console.log(`Sunucu ${port} portunda başlatıldı`);
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post',(req,res) => {
+    res.render('add_post')
+});
+
+app.get('/post',(req,res) => {
+    res.render('post')
 })
 
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı`);
+});
