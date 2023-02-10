c5851700a6ead4a2433e5ca26b1ec154e808f7c3
1kivanc <kivanc0556@gmail.com>
2023-02-10 15:28:31 +0300

    mongoose crud

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Blog = require('./models/blog');
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
 const app = express();
+
+
+
+// Connect DB
+mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/cleanblog-test-db');
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
 const port = 3000;
 const blog = {
   id: 1,
   title: 'Blog title',
   description: 'Blog description',
 };
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const blogs = await Blog.find({})
+  res.render('index',{
+    blogs
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -28,7 +43,12 @@ app.get('/add_post',(req,res) => {
 
 app.get('/post',(req,res) => {
     res.render('post')
-})
+});
+
+app.post('/blogs', async (req,res) => {
+  await Blog.create(req.body);
+  res.redirect('/');
+});
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı`);
